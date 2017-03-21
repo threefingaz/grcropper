55ff5ca465f6a30cd22d386ee11671cfa59f74f4
Al Nikitin <nikitin010@gmail.com>
2017-03-21 15:46:10 +0300

    fixed submodule issue

---
+++ src/components/roppe/Cropper.js
@@ -0,0 +1,183 @@
+import React, { Component } from 'react';
+
+class Cropper extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            cropping:   false,
+            img_size:   { width: 0, height: 0 },
+            delta:      { top: 0, left: 0 },
+            canvas:     { top: 0, left: 0 },
+            offset:     { top: 0, left: 0 }
+        };
+
+        this.draw = this.draw.bind(this);
+        this.crop = this.crop.bind(this);
+        this.startCropping = this.startCropping.bind(this);
+        this.stopCropping = this.stopCropping.bind(this);
+    }
+
+    componentDidMount() {
+        let state = {...this.state};
+
+        state.img_size.width = this.props.img_element.width;
+        state.img_size.height = this.props.img_element.height;
+
+        this.setState(state);
+        this.draw();
+        this.props.setImage(this.canvas.toDataURL());
+    }
+
+    componentDidUpdate() {
+        this.draw();
+    }
+
+    draw() {
+        const ctx = this.canvas.getContext('2d');
+        const img_size = { ...this.state.img_size };
+        const offset = { ...this.state.offset };
+
+        ctx.clearRect(0, 0, this.props.width, this.props.height);
+
+        ctx.drawImage(
+            // image,
+            this.props.img_element,
+            // sx, sy, sWidth, sHeight, dx, dy,
+            -offset.left, -offset.top, img_size.width, img_size.height,
+            // dWidth, dHeight
+            0, 0, img_size.width, img_size.height
+        );
+
+        this.props.poseImage({
+            top: offset.top,
+            left: offset.left,
+        });
+    }
+
+    crop(moveEvent) {
+        if (!this.state.cropping) return;
+
+        const { img_size, delta, offset, canvas } = { ...this.state };
+        const width = this.props.width;
+        const height = this.props.height;
+
+        const top = (
+            moveEvent.pageY - window.scrollY
+            - delta.top
+            - canvas.top
+        );
+        const left = (
+            moveEvent.pageX - window.scrollX
+            - delta.left
+            - canvas.left
+        );
+
+        function getTop() {
+            const too_height = (
+                canvas.top + top + img_size.height < canvas.top + height
+            );
+            const too_low = top > 0;
+
+            if (too_height) {
+                return height - img_size.height;
+            } else if (too_low) {
+                return 0;
+            } else {
+                return top;
+            }
+        }
+
+        function getLeft() {
+            const too_left = (
+                canvas.left + left + img_size.width < canvas.left + width
+            );
+            const too_right = left > 0;
+
+            if (too_left) {
+                return width - img_size.width;
+            } else if (too_right) {
+                return 0;
+            } else {
+                return left;
+            }
+        }
+
+        offset.top = getTop();
+        offset.left = getLeft();
+
+        this.setState({ offset });
+        this.draw();
+    };
+
+    startCropping(clickEvent) {
+        clickEvent.preventDefault();
+        const canvas = this.canvas.getBoundingClientRect();
+        const offset = { ...this.state.offset };
+
+        // Calculate pixels to add to cursor coordinates
+        let top = (
+            // click position Y
+            clickEvent.pageY - window.scrollY
+            // image offset from canvas top
+            - canvas.top
+            - offset.top
+        );
+        let left = (
+            // click position X
+            clickEvent.pageX - window.scrollX
+            // image offset from canvas Left
+            - canvas.left
+            - offset.left
+        );
+
+        this.setState({
+            cropping: true,
+            delta: {
+                top, left
+            },
+            canvas: {
+                top: canvas.top,
+                left: canvas.left
+            }
+        });
+
+        document.onmousemove = event => this.crop(event);
+        document.onmouseup = () => this.stopCropping();
+    };
+
+    stopCropping() {
+        this.setState({ cropping: false });
+        this.props.setImage(this.canvas.toDataURL());
+        document.onmousemove = null;
+        document.onmouseup = null;
+    };
+
+    render() {
+        return (
+                <canvas className="roppe-cropper"
+                        width={ this.props.width }
+                        height={ this.props.height }
+                        onMouseDown={ event => this.startCropping(event) }
+                        onMouseMove={ event => this.crop(event) }
+                        onMouseUp={ () => this.stopCropping() }
+                        ref={ canvas => this.canvas = canvas }>
+                </canvas>
+        );
+    }
+}
+
+Cropper.defaultProps = {
+    width: 800,
+    height: 600
+};
+
+Cropper.propTypes = {
+    width: React.PropTypes.number,
+    height: React.PropTypes.number,
+    poseImage: React.PropTypes.func,
+    setImage: React.PropTypes.func,
+    img_element: React.PropTypes.object
+};
+
+export default Cropper;
---
+++ src/components/roppe/index.js
@@ -0,0 +1,120 @@
+import React, {Component} from 'react';
+
+import Dropzone from 'react-dropzone';
+import Cropper from './Cropper';
+
+import './roppe.css';
+
+class Roppe extends Component {
+    constructor(props) {
+        super();
+
+        this.state = {
+            src: false,
+            croppedImage: false
+        };
+
+        this.setPreview = this.setPreview.bind(this);
+        this.clearImage = this.clearImage.bind(this);
+        this.setCroppedImage = this.setCroppedImage.bind(this);
+        this.poseImage = this.poseImage.bind(this);
+        this.drawCropper = this.drawCropper.bind(this);
+    }
+
+    componentWillMount() {
+        this.setState({
+            size: {
+                width: this.props.width,
+                height: this.props.height
+            }
+        });
+    }
+
+    setPreview(blob) {
+        this.setState({ src: blob });
+    }
+
+    setCroppedImage(url) {
+        this.setState({ croppedImage: url });
+    }
+
+    clearImage() {
+        this.setState({
+            src: false,
+            is_loaded: false,
+            croppedImage: false
+        });
+    }
+
+    poseImage(coords) {
+        this.img_element.style.top = `${coords.top}px`;
+        this.img_element.style.left = `${coords.left}px`;
+    }
+
+    drawCropper() {
+        this.setState({ is_loaded: true });
+    }
+
+    render() {
+        const { src } = { ...this.state };
+        const classes = () => {
+            const from_props = this.props.className ? `${this.props.className} ` : ``;
+            const from_state = src ? `roppe-cropper` : `roppe-dropper`;
+
+            return `${from_props}${from_state}`;
+        };
+        const dropper = <Dropzone
+                            className="drop-area"
+                            activeClassName="drop-area__active"
+                            onDrop={ (files) => this.setPreview(files[0].preview) }
+                            multiple={ false }
+                            accept="image/*,.png,.gif,.jpg">
+                            { this.props.instruction }
+                        </Dropzone>;
+        const image = <img alt="Original"
+                           className="img-original"
+                           onLoad={ () => this.drawCropper() }
+                           ref={ img => this.img_element = img }
+                           src={ src } />;
+        const cropper = <Cropper
+                            width={this.props.width}
+                            height={this.props.height}
+                            img_element={ this.img_element }
+                            setImage={ this.setCroppedImage }
+                            poseImage={ this.poseImage } />;
+        const download = <a className="btn btn__download"
+                            href={this.state.croppedImage}
+                            type="button">
+                            <span className="btn-icon">
+                                ⬇
+                            </span>
+                            &nbsp;Download
+                        </a>;
+        const clear = <button className="btn btn__clear"
+                              type="button"
+                              onClick={ this.clearImage }>
+                            ×
+                      </button>;
+
+        return (
+            <div className={`roppe ${classes()}`}
+                 style={{
+                     width: `${ this.props.width }px`,
+                     height: `${ this.props.height }px`
+                 }}>
+                { this.state.croppedImage ? download : null }
+                { this.state.is_loaded ? clear : null }
+                { this.state.src ? image : dropper }
+                { this.state.is_loaded ? cropper : null }
+            </div>
+        );
+    }
+}
+
+Roppe.defaultProps = {
+    width: 800,
+    height: 600,
+    instruction: 'Drop image file here, or click to select'
+};
+
+export default Roppe;
